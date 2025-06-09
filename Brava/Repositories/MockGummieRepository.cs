e4d235ab63bb45ccfb22a69a9304d8cf57d03672
blake7689 <blakec7689@gmail.com>
2025-06-09 17:13:14 +0100

    product home section changes

--- Brava/Repositories/MockGummieRepository.cs
+++ Brava/Repositories/MockGummieRepository.cs
@@ -14,16 +14,7 @@ namespace Brava.Repositories
                     Price=19.99M,
                     Description="Strawberry Flavoured Creatine Gummies",
                     Size="1,25g",
-                    ImagePath="images/product1.jpg",
-                    AllergyInformation="~Allergy Info~"},
-
-                new Gummie {
-                    GummieID = 2,
-                    Name= "Strawberry",
-                    Price=19.99M,
-                    Description="Strawberry Flavoured Creatine Gummies",
-                    Size="1,25g",
-                    ImagePath="images/product1.jpg",
+                    ImagePath="images/300x300noBG.png",
                     AllergyInformation="~Allergy Info~"}
             };
     }
