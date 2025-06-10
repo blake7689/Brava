16756ffa9d136360bef52642e3eada5c60d57d5d
blake7689 <blakec7689@gmail.com>
2025-06-11 00:51:53 +0100

    Updates

--- Brava/Repositories/MockGummieRepository.cs
+++ Brava/Repositories/MockGummieRepository.cs
@@ -15,6 +15,15 @@ namespace Brava.Repositories
                     Description="Strawberry Flavoured Creatine Gummies",
                     Size="1,25g",
                     ImagePath="images/300x300noBG.png",
+                    AllergyInformation="~Allergy Info~"},
+
+                new Gummie {
+                    GummieID = 2,
+                    Name= "Strawberry..Again",
+                    Price=19.99M,
+                    Description="Strawberry Flavoured Creatine Gummies",
+                    Size="1,25g",
+                    ImagePath="images/300x300noBG.png",
                     AllergyInformation="~Allergy Info~"}
             };
     }
